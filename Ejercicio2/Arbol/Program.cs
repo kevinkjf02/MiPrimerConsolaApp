ac55666dea1400543d6d4de8080fdbf53f1773d2
kevinkjf02 <kevinkjf02@gmail.com>
2021-06-23 20:16:19 -0500

    Ordenar las líneas de código

--- Ejercicio2/Arbol/Program.cs
+++ Ejercicio2/Arbol/Program.cs
@@ -1,71 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 namespace Arbol.app
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            AreaPolitica areaPolitica = new AreaPolitica
+            AreaPolitica areaPolitica = new()
             {
-
                 nombre = "Ecuador",
                 TipoArea = TipoArea.Pais,
                 AreaPoliticas = new List<AreaPolitica>
                 {
-                new AreaPolitica
+                    new AreaPolitica
+                    { 
+                        nombre = "Pichincha",
+                        TipoArea = TipoArea.Provincia,
+                        AreaPoliticas = new List<AreaPolitica>
                         {
-                            nombre = "Pichincha",
-                            TipoArea = TipoArea.Provincia,
-                            AreaPoliticas = new List<AreaPolitica>
+                            new AreaPolitica
                             {
-                                new AreaPolitica
+                                nombre = "Quito",
+                                TipoArea = TipoArea.Canton,
+                                AreaPoliticas = new List<AreaPolitica>
                                 {
-                                    nombre = "Quito",
-                                    TipoArea = TipoArea.Canton,
-                                    AreaPoliticas = new List<AreaPolitica>
+                                    new AreaPolitica
                                     {
-                                        new AreaPolitica
-                                        {
-                                            nombre = "La Merced",
-                                            TipoArea = TipoArea.Parroquia,
-                                            Habitantes=80
-
-                                        },
-                                        new AreaPolitica
-                                        {
-                                            nombre = "Alangasi",
-                                            TipoArea = TipoArea.Parroquia,
-                                            Habitantes= 50
-                                        }
+                                        nombre = "La Merced",
+                                        TipoArea = TipoArea.Parroquia,
+                                        Habitantes=80
+                                    },
+                                    new AreaPolitica
+                                    {
+                                        nombre = "Alangasi",
+                                        TipoArea = TipoArea.Parroquia,
+                                        Habitantes= 50
                                     }
-                                },
-                                new AreaPolitica
+                                }
+                            },
+                            new AreaPolitica
+                            {
+                                nombre = "Rumiñahui",
+                                TipoArea = TipoArea.Canton,
+                                AreaPoliticas = new List<AreaPolitica>
                                 {
-                                    nombre = "Rumiñahui",
-                                    TipoArea = TipoArea.Canton,
-                                    AreaPoliticas = new List<AreaPolitica>
+                                    new AreaPolitica
                                     {
-                                        new AreaPolitica
-                                        {
-                                            nombre = "Conocoto",
-                                            TipoArea = TipoArea.Parroquia
-                                        },
-                                        new AreaPolitica
-                                        {
-                                            nombre = "ASAS",
-                                            TipoArea = TipoArea.Parroquia,
-                                            Habitantes=10
-                                           
-                                        }
+                                        nombre = "Conocoto",
+                                        TipoArea = TipoArea.Parroquia
+                                    },
+                                    new AreaPolitica
+                                    {
+                                        nombre = "Nayón",
+                                        TipoArea = TipoArea.Parroquia,
+                                        Habitantes=10
+
                                     }
                                 }
                             }
                         }
+                    }
                 }
 
             };
-            System.Console.WriteLine (areaPolitica.ObtenerNumeroHabitantes());
+            System.Console.WriteLine(areaPolitica.ObtenerNumeroHabitantes());
         }
     }
 }
